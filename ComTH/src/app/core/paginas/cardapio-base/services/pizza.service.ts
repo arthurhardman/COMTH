8d7c60a10fa12cfadd4989d3a3066b5b978dd0eb
Arthur Paula <arthur.paula@autoglass.com.br>
2024-09-03 08:58:15 -0300

    CTH

--- ComTH/src/app/core/paginas/cardapio-base/services/pizza.service.ts
+++ ComTH/src/app/core/paginas/cardapio-base/services/pizza.service.ts
@@ -10,67 +10,73 @@ export class PizzaService {
     const pizzas: Array<IPizza> = [
       {
         Nome: 'Alho Frito',
-        Preco: 'R$ 38,00',
+        Preco: 'R$ 40,00',
         Foto: 'https://files.menudino.com/cardapios/57354/11.jpg',
         Componentes: 'Massa de longa fermentação, molho de tomate caseiro, mozarela, alho frito e manjericão fresco.'
       },
       {
         Nome: 'Banana Nevada',
-        Preco: 'R$ 38,00',
+        Preco: 'R$ 42,90',
         Foto: 'https://files.menudino.com/cardapios/57354/13.jpg',
         Componentes: 'Massa de longa fermentação, banana, chocolate branco, doce de leite e canela.'
       },
       {
         Nome: 'Bacon Garlic',
-        Preco: 'R$ 42,00',
+        Preco: 'R$ 44,00',
         Foto: 'https://files.menudino.com/cardapios/57354/9.jpg',
         Componentes: 'Massa de longa fermentaçao, creme de alho confitado, mozarela, alho poró e bacon fatiado.'
       },
       {
         Nome: 'Calabresa',
-        Preco: 'R$ 41,90',
+        Preco: 'R$ 45,00',
         Foto: 'https://files.menudino.com/cardapios/57354/3.jpg',
         Componentes: 'Massa de longa fermentação, molho de tomate caseiro, mozarela, calabresa e cebola.'
       },
       {
         Nome: 'Calabresa Premium',
-        Preco: 'R$ 47,50',
+        Preco: 'R$ 50,00',
         Foto: 'https://files.menudino.com/cardapios/57354/4.jpg',
         Componentes: 'Massa de longa fermentação, molho de tomate caseiro, mozarela, alho poró, calabresa processada e requeijão vigor.'
       },
       {
         Nome: 'Calábria',
-        Preco: 'R$ 45,50',
+        Preco: 'R$ 49,90',
         Foto: 'https://files.menudino.com/cardapios/57354/5.jpg',
         Componentes: 'Massa de longa fermentação, molho de tomate caseiro, mozarela, calabresa em rodelas, cream cheese, manjericão e mel.'
       },
       {
         Nome: 'ComTH',
-        Preco: 'R$ 43,50',
+        Preco: 'R$ 46,00',
         Foto: 'https://files.menudino.com/cardapios/57354/6.jpg',
         Componentes: 'Massa de longa fermentação, molho de tomate caseiro, mozarela, calabresa processada, alho frito, azeitona e molho agridoce.'
       },
       {
         Nome: 'Frango no Vigor',
-        Preco: 'R$ 49,00',
+        Preco: 'R$ 52,90',
         Foto: 'https://files.menudino.com/cardapios/57354/8.jpg',
         Componentes: 'Massa de longa fermentação, molho de tomate caseiro, mozarela, frango desfiado e requeijão vigor.'
       },
       {
-        Nome: 'Portuga',
-        Preco: 'R$ 45,00',
-        Foto: 'https://files.menudino.com/cardapios/57354/7.jpg',
-        Componentes: 'Massa de longa fermentação, molho de tomate caseiro, presunto, farofa de bacon, mozarela, cebola e azeitona.'
+        Nome: 'Gorgon Ramsey',
+        Preco: 'R$ 44,00',
+        Foto: 'https://files.menudino.com/cardapios/57354/10.jpg',
+        Componentes: 'Massa de longa fermentação, molho bechamel, mozarela, gorgonzola, cebola caramelizada e manjericão.'
       },
       {
         Nome: 'Mozarela',
-        Preco: 'R$ 36,00',
+        Preco: 'R$ 42,00',
         Foto: 'https://files.menudino.com/cardapios/57354/12.jpg',
         Componentes: 'Massa de longa fermentação, molho de tomate caseiro, mozarela e orégano.'
       },
       {
+        Nome: 'Portuga',
+        Preco: 'R$ 48,50',
+        Foto: 'https://files.menudino.com/cardapios/57354/7.jpg',
+        Componentes: 'Massa de longa fermentação, molho de tomate caseiro, presunto, farofa de bacon, mozarela, cebola e azeitona.'
+      },
+      {
         Nome: 'Rainha Margherita',
-        Preco: 'R$ 36,00',
+        Preco: 'R$ 44,50',
         Foto: 'https://files.menudino.com/cardapios/57354/10.jpg',
         Componentes: 'Massa de longa fermentação, molho de tomate caseiro, mozarela, tomate em cubos e manjericão fresco.'
       }
